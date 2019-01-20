aa832ca6bcfc8b22345cd1fc7439f50087d67442
Daniel Cloud <daniel@danielcloud.org>
2019-01-20 03:20:38 -0500

    Custom color support (#3)
    * Make colors an Enum, use it to set color options cli arg.
    
    * Handle Color enum values as well as raw values in make_bitmap
    
    * make_bitmap yield needs explicit grouping due to conditional
    
    * Parenthesis were ambiguous, need to pass a list to tuple()
    
    * Fix kwarg name, begin addition of rgb option
    
    * Add more verbose output
    
    * More print statement fun
    
    * CLI argument is a string and will need to be used to look up a color
    value
    
    * Fix string format print statement
    
    * Remove default from color cli arg so grb option will be read

--- piledclock/cli.py
+++ piledclock/cli.py
@@ -4,6 +4,7 @@ import time
 import unicornhathd as uhd
 
 from .core import split_digits, make_bitmap, print_bitmap
+from .colors import Color
 from .chars import NUMBERS
 from .conf import (
     DISPLAY_ROTATION,
@@ -13,8 +14,18 @@ from .conf import (
     QUAD_SIZE,
 )
 
+color_choices = tuple([c.name for c in Color])
+
 
 def run(args):
+
+    user_color = None
+    if args.colorname:
+        user_color = Color[args.colorname]
+    elif args.rgb:
+        user_color = tuple(args.rgb)
+    if args.verbose > 0:
+        print("Use color {}".format(user_color))
     uhd.off()
     uhd.rotation(args.rotation)
     uhd.brightness(1.0)
@@ -25,14 +36,18 @@ def run(args):
     quadrants = ((x % 2 * QUAD_SIZE, x // 2 * QUAD_SIZE) for x in range(4))
 
     for n, pos in zip(digits, quadrants):
-        bitmap = make_bitmap(NUMBERS[n - 1], offset=pos)
+        if args.verbose > 1:
+            print(
+                pos, user_color.value if isinstance(user_color, Color) else user_color
+            )
+        bitmap = make_bitmap(NUMBERS[n - 1], offset=pos, color_choice=user_color)
         if args.verbose > 1:
             print(bitmap)
         print_bitmap(uhd, bitmap)
 
     if args.verbose > 0:
         print("Hour {:02d}".format(dt.hour))
-        print("Minute {:02d}", dt.minute)
+        print("Minute {:02d}".format(dt.minute))
 
     if args.fade:
         if args.verbose > 1:
@@ -77,7 +92,9 @@ def main():
     parser = argparse.ArgumentParser(description="Display time on LED clock")
     parser.add_argument("--verbose", "-v", action="count", default=0)
     parser.add_argument("--rotation", type=int, default=DISPLAY_ROTATION)
-    parser.add_argument("--color", choices=["RED", "GREEN", "BLUE"])
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--color", dest="colorname", choices=color_choices)
+    group.add_argument("--rgb", nargs=3, type=int)
     parser.add_argument("--no-fade", action="store_false", dest="fade")
     parser.add_argument(
         "--duration",
