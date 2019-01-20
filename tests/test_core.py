69bb6ff42836c954884c4f6fc364a07c437e4017
Daniel Cloud <daniel@danielcloud.org>
2019-01-20 18:14:31 -0500

    Update tests to mock unicornhathd so we can't test without the hardware

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,12 +1,13 @@
+import sys
 from unittest.mock import Mock, call
 
-from piledclock.core import print_bitmap, split_digits
-
-
+sys.modules["unicornhathd"] = Mock()
 mock_display = Mock()
 
 
 def test_print_bitmap():
+    from piledclock.core import print_bitmap
+
     bitmap = [((0, 0), (255, 255, 255)), ((0, 1), (255, 255, 0))]
     print_bitmap(mock_display, bitmap)
 
@@ -16,5 +17,7 @@ def test_print_bitmap():
 
 
 def test_split_digits():
+    from piledclock.core import split_digits
+
     assert split_digits(12) == (1, 2)
     assert split_digits(6) == (0, 6)
